a1974ae882d0c579fc2885f9adb15e0c5396f505
BobbyRockers <rrockers@gmail.com>
2018-01-31 20:26:53 -0600

    More examples

--- CIS2013_Week03_Lab01/flowControl.cpp
+++ CIS2013_Week03_Lab01/flowControl.cpp
@@ -3,23 +3,32 @@ using namespace std;
 
 int main() {
 	char continueProgram = 'x';
-	
-	do {
 
-		cout << "Do you want to continue (y/n)" << endl;
+	for (int i = 99; i >= 0; i--) {
+		if (i == 1) {
+			cout << i << " bottle of beer on the wall" << endl;
+		}
+		else if (i == 0) {
+			cout << "No more bottles of beer on the wall" << endl;
+		}		else {			cout << i << " bottles of beer on the wall" << endl;		}
+	}
+	
+	//do {
+
+	//	cout << "Do you want to continue (y/n)" << endl;
 		cin >> continueProgram;
 
-		switch (continueProgram) {
-			case 'y':
-				cout << "Going to continue" << endl;
-				break;
-			case 'n':
-				cout << "Going to exit" << endl;
-				break;
-			default:
-				continueProgram = 'y';
-		}
-	} while (continueProgram == 'y');
+	//	switch (continueProgram) {
+	//		case 'y':
+	//			cout << "Going to continue" << endl;
+	//			break;
+	//		case 'n':
+	//			cout << "Going to exit" << endl;
+	//			break;
+	//		default:
+	//			continueProgram = 'y';
+	//	}
+	//} while (continueProgram == 'y');
 	
 	return 0;
 }
