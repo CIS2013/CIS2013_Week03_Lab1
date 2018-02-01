366421d1280d6d6c9e815e2419439054c1fd864a
BobbyRockers <rrockers@gmail.com>
2018-01-31 21:00:06 -0600

    Random Number Example

--- CIS2013_Week03_Lab01/flowControl.cpp
+++ CIS2013_Week03_Lab01/flowControl.cpp
@@ -1,34 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 int main() {
-	char continueProgram = 'x';
-
-	for (int i = 99; i >= 0; i--) {
-		if (i == 1) {
-			cout << i << " bottle of beer on the wall" << endl;
-		}
-		else if (i == 0) {
-			cout << "No more bottles of beer on the wall" << endl;
-		}		else {			cout << i << " bottles of beer on the wall" << endl;		}
-	}
 	
-	//do {
-
-	//	cout << "Do you want to continue (y/n)" << endl;
-		cin >> continueProgram;
-
-	//	switch (continueProgram) {
-	//		case 'y':
-	//			cout << "Going to continue" << endl;
-	//			break;
-	//		case 'n':
-	//			cout << "Going to exit" << endl;
-	//			break;
-	//		default:
-	//			continueProgram = 'y';
-	//	}
-	//} while (continueProgram == 'y');
+	char cont;
+	srand(time(NULL));
 	
+	int num1 = rand() % 10 + 1;
+	cout << "your random number is " << num1;
+	cin >> cont;
+
 	return 0;
 }
